28f52345fb98f4d69c809d04327b6d0b68d07550
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-28 20:50:20 +0000

    Automatically backed up by Learn

--- spec/conference_badges_spec.rb
+++ spec/conference_badges_spec.rb
@@ -4,52 +4,6 @@ describe 'conference_badges' do
 
   let(:name) {"Arel"}
   let(:attendees) {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
-  let(:badges) {[
-                  "Hello, my name is Edsger.",
-                  "Hello, my name is Ada.",
-                  "Hello, my name is Charles.",
-                  "Hello, my name is Alan.",
-                  "Hello, my name is Grace.",
-                  "Hello, my name is Linus.",
-                  "Hello, my name is Matz."
-               ]}
-
-  let(:room_assignments) {[
-                            "Hello, Edsger! You'll be assigned to room 1!",
-                            "Hello, Ada! You'll be assigned to room 2!",
-                            "Hello, Charles! You'll be assigned to room 3!",
-                            "Hello, Alan! You'll be assigned to room 4!",
-                            "Hello, Grace! You'll be assigned to room 5!",
-                            "Hello, Linus! You'll be assigned to room 6!",
-                            "Hello, Matz! You'll be assigned to room 7!"
-                         ]}
-
-# This funny looking <<-TEXT thing is called heredoc. It's a multi-line string
-# delimiter that makes it really easy for us to write multi-line strings in the
-# middle of code. The TEXT part is arbitrary too.
-# It could easily have been <<-BLAHBLAH. The only requirement is that you use
-# the same word to end the multi-line string.
-
-# For more info about heredocs, see this link:
-# http://en.wikibooks.org/wiki/Ruby_Programming/Here_documents
-
-  let(:badges_and_room_assignments) {<<-TEXT
-Hello, my name is Edsger.
-Hello, my name is Ada.
-Hello, my name is Charles.
-Hello, my name is Alan.
-Hello, my name is Grace.
-Hello, my name is Linus.
-Hello, my name is Matz.
-Hello, Edsger! You'll be assigned to room 1!
-Hello, Ada! You'll be assigned to room 2!
-Hello, Charles! You'll be assigned to room 3!
-Hello, Alan! You'll be assigned to room 4!
-Hello, Grace! You'll be assigned to room 5!
-Hello, Linus! You'll be assigned to room 6!
-Hello, Matz! You'll be assigned to room 7!
-TEXT
-}
 
  end
 
