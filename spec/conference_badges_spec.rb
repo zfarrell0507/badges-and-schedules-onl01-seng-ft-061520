0063eaf6c34c6e0f5dbe16d30e08ecc1034393af
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-28 20:48:55 +0000

    Automatically backed up by Learn

--- spec/conference_badges_spec.rb
+++ spec/conference_badges_spec.rb
@@ -66,37 +66,5 @@ TEXT
 
   end
 
-  describe '#assign_rooms' do
-
-    # Question 3
-
-    it 'should return a list of welcome messages and room assignments' do
-      expect(assign_rooms(attendees)).to eq(room_assignments)
-    end
-    it 'should not hard-code the response' do
-      expect(assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
-    end
-
-  end
-
-  describe '#printer' do
-
-    # Question 4
-    # The method `printer` should output first the results of the batch_badge_creator method and then of the assign_rooms method to the screen - this way you can output
-    # the badges and room assignments one at a time.
-    # To make this test pass, make sure you are iterating through your badges and room assignments lists.
-
-    it 'should puts the list of badges and room_assignments' do
-      badges_and_room_assignments.each_line do |line|
-        # $stdout is a Ruby global varibale that represents the current standard output.
-        # In this case, the standard output is your terminal screen. This test, then,
-        # is checking to see whether or not your terminal screen receives the correct
-        # printed output.
-        expect($stdout).to receive(:puts).with(line.chomp)
-      end
-      printer(attendees)
-    end
-
-  end
-
+ 
 end
